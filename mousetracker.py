7643703b6e47425b2fe7c364cf487ba27a9d1692
Jakob Høy <gatlingjakob@hotmail.com>
2020-02-11 15:37:18 +0100

    added comments

--- mousetracker.py
+++ mousetracker.py
@@ -1,23 +1,24 @@
 import pygame, time, math
 
-#We will make a window with pygame to simulate a browser window
-#Pygame setup
+# We will make a window with pygame to simulate a browser window
+# Pygame setup
 pygame.init()
 size = width, height = 300, 300
 speed = [2, 2]
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
-#Amount of time between start and end coordinates are logged
+# Amount of time between start and end coordinates are logged
 waitingTime = 2
+
+# List to store mouse speed values for calculation of average speed
 mouseLog = []
 
 def Average(lst): 
     return sum(lst) / len(lst) 
 
 def mouseSpeed():
-
-    # Start
+    # Get start position
     data = pygame.mouse.get_pos()
     print("Start position: {0}".format(data))
     x1 = data[0] 
@@ -26,11 +27,11 @@ def mouseSpeed():
 
     time.sleep(waitingTime)
 
-    # Clear pygame event data
+    # Clear pygame event data to store new value
     for event in pygame.event.get():
         pass     
 
-    # End
+    # Get end position 
     data = pygame.mouse.get_pos()
     print("End position: {0}".format(data))
     x2 = data[0]
@@ -42,20 +43,22 @@ def mouseSpeed():
     deltaY = y2 - y1
     distance = math.sqrt( math.pow(deltaX, 2) + math.pow(deltaY, 2) ) # Distance between 2 points
 
-    # Get the change in time
+    # Get the change in time 
+    # the variable waitingTime would serve the same purpose, 
+    # but chose to leave deltaZ in, in case I want to reuse the algorithm later
+    # (Plus, there is a small, albeit insignificant, difference in milliseconds)
     deltaZ = z2 - z1
 
-    # Get distance/change in time
+    # Get distance/time (pixels/seconds)
     mouseSpeedUnits = math.ceil(distance/deltaZ)
     mouseLog.append(mouseSpeedUnits)
     # Print out the speed
     print ("I've traveled {0} Mouse Speed Units in the last {1} seconds".format(mouseSpeedUnits, waitingTime))
     average = Average(mouseLog)
     print("Current Average Speed:{0}".format(average))
-
     print(" ")
     
-    # Clear pygame event data
+    # Clear pygame event data to store new value
     for event in pygame.event.get():
         pass            
 
